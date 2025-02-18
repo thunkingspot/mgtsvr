578a80322f660a0c5666328c30c38aa73cb4b929
rich <dev@thunkingspot.com>
2025-02-18 00:03:22 +0000

    fix path issues

--- mgt/deployrepo.sh
+++ mgt/deployrepo.sh
@@ -8,11 +8,14 @@
     ssh-add ~/.ssh/id_ed25519
 
     # Clone git repo to temporary directory
+    CURRENT_DIR=$(pwd)
     TEMP_DIR=$(mktemp -d)
     git clone $2 $TEMP_DIR
+    cd $TEMP_DIR
 
     # Run the deployment script in the repo
     /bin/bash $TEMP_DIR/$3 $DEBUG_MODE
 
     # Clean up the temporary directory
+    cd $CURRENT_DIR
     rm -rf $TEMP_DIR
