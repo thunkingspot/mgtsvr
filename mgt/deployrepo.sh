54bf7282eeaf47be4a547b099cb24c2a1e5532ed
rich <dev@thunkingspot.com>
2025-03-12 00:24:35 +0000

    make sure output is non-interactive

--- mgt/deployrepo.sh
+++ mgt/deployrepo.sh
@@ -53,16 +53,16 @@ elif [ "$PHASE" == "deploy-inactive" ]; then
   # /bin/bash mgt/deployrepo.sh aqua-app git@github.com:thunkingspot/aqua.git mgt deploy-inactive deploy.sh false
 
   # Determine the inactive listener ARN (operating on HTTP port 81)
-  INACTIVE_LISTENER_ARN=$(aws elbv2 describe-listeners --load-balancer-arn $ELB_ARN --region $AWS_REGION --query 'Listeners[?Port==`81`].ListenerArn' --output text)
+  INACTIVE_LISTENER_ARN=$(aws elbv2 describe-listeners --load-balancer-arn $ELB_ARN --region $AWS_REGION --query 'Listeners[?Port==`81`].ListenerArn' --output text --no-paginate)
 
   # Determine the target group associated with the inactive listener
-  INACTIVE_TARGET_GROUP=$(aws elbv2 describe-rules --listener-arn $INACTIVE_LISTENER_ARN --region $AWS_REGION --query 'Rules[*].Actions[*].TargetGroupArn' --output text)
+  INACTIVE_TARGET_GROUP=$(aws elbv2 describe-rules --listener-arn $INACTIVE_LISTENER_ARN --region $AWS_REGION --query 'Rules[*].Actions[*].TargetGroupArn' --output text --no-paginate)
 
   # Query the inactive target group to get instance IDs
-  INSTANCE_IDS=$(aws elbv2 describe-target-health --target-group-arn $INACTIVE_TARGET_GROUP --region $AWS_REGION --query 'TargetHealthDescriptions[*].Target.Id' --output text)
+  INSTANCE_IDS=$(aws elbv2 describe-target-health --target-group-arn $INACTIVE_TARGET_GROUP --region $AWS_REGION --query 'TargetHealthDescriptions[*].Target.Id' --output text --no-paginate)
 
   # Get the IP addresses of the instances
-  INSTANCE_IPS=$(aws ec2 describe-instances --instance-ids $INSTANCE_IDS --region $AWS_REGION --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
+  INSTANCE_IPS=$(aws ec2 describe-instances --instance-ids $INSTANCE_IDS --region $AWS_REGION --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text --no-paginate)
 
   # Deploy the Docker image to each instance
   for IP in $INSTANCE_IPS; do
@@ -75,23 +75,17 @@ elif [ "$PHASE" == "swap" ]; then
   # /bin/bash mgt/deployrepo.sh aqua-app git@github.com:thunkingspot/aqua.git mgt swap na false
 
   # Determine the active listener ARN (operating on HTTP port 80)
-  # aws elbv2 describe-listeners --load-balancer-arn "arn:aws:elasticloadbalancing:us-west-2:164348132997:loadbalancer/app/THUNKINGSPOTLLC-SVCS/b3d24b7dd2df1dcf" --region "us-west-2" --query 'Listeners[?Port==`80`].ListenerArn' --output text
-  ACTIVE_LISTENER_ARN=$(aws elbv2 describe-listeners --load-balancer-arn $ELB_ARN --region $AWS_REGION --query 'Listeners[?Port==`80`].ListenerArn' --output text)
+  ACTIVE_LISTENER_ARN=$(aws elbv2 describe-listeners --load-balancer-arn $ELB_ARN --region $AWS_REGION --query 'Listeners[?Port==`80`].ListenerArn' --output text --no-paginate)
   # Determine the inactive listener ARN (operating on HTTP port 81)
-  # aws elbv2 describe-listeners --load-balancer-arn "arn:aws:elasticloadbalancing:us-west-2:164348132997:loadbalancer/app/THUNKINGSPOTLLC-SVCS/b3d24b7dd2df1dcf" --region "us-west-2" --query 'Listeners[?Port==`81`].ListenerArn' --output text
-  INACTIVE_LISTENER_ARN=$(aws elbv2 describe-listeners --load-balancer-arn $ELB_ARN --region $AWS_REGION --query 'Listeners[?Port==`81`].ListenerArn' --output text)
+  INACTIVE_LISTENER_ARN=$(aws elbv2 describe-listeners --load-balancer-arn $ELB_ARN --region $AWS_REGION --query 'Listeners[?Port==`81`].ListenerArn' --output text --no-paginate)
 
   # Determine the target group associated with the inactive listener
-  # aws elbv2 describe-rules --listener-arn "arn:aws:elasticloadbalancing:us-west-2:164348132997:listener/app/THUNKINGSPOTLLC-SVCS/b3d24b7dd2df1dcf/dbb8d3ee6711842b" --region "us-west-2" --query 'Rules[*].Actions[*].TargetGroupArn' --output text
-  INACTIVE_TARGET_GROUP=$(aws elbv2 describe-rules --listener-arn $INACTIVE_LISTENER_ARN --region $AWS_REGION --query 'Rules[*].Actions[*].TargetGroupArn' --output text)
+  INACTIVE_TARGET_GROUP=$(aws elbv2 describe-rules --listener-arn $INACTIVE_LISTENER_ARN --region $AWS_REGION --query 'Rules[*].Actions[*].TargetGroupArn' --output text --no-paginate)
   # Determine the target group associated with the active listener
-  # aws elbv2 describe-rules --listener-arn "arn:aws:elasticloadbalancing:us-west-2:164348132997:listener/app/THUNKINGSPOTLLC-SVCS/b3d24b7dd2df1dcf/f2c5e056e61768e3" --region "us-west-2" --query 'Rules[*].Actions[*].TargetGroupArn' --output text
-  ACTIVE_TARGET_GROUP=$(aws elbv2 describe-rules --listener-arn $ACTIVE_LISTENER_ARN --region $AWS_REGION --query 'Rules[*].Actions[*].TargetGroupArn' --output text)
+  ACTIVE_TARGET_GROUP=$(aws elbv2 describe-rules --listener-arn $ACTIVE_LISTENER_ARN --region $AWS_REGION --query 'Rules[*].Actions[*].TargetGroupArn' --output text --no-paginate)
 
   # Modify the rules to swap the target groups
-  # aws elbv2 modify-listener --listener-arn "arn:aws:elasticloadbalancing:us-west-2:164348132997:listener/app/THUNKINGSPOTLLC-SVCS/b3d24b7dd2df1dcf/f2c5e056e61768e3" --default-actions Type=forward,TargetGroupArn="arn:aws:elasticloadbalancing:us-west-2:164348132997:targetgroup/aqua-green/24e80198d3d6b4d5" --region "us-west-2"
   aws elbv2 modify-listener --listener-arn $ACTIVE_LISTENER_ARN --default-actions Type=forward,TargetGroupArn=$INACTIVE_TARGET_GROUP --region $AWS_REGION
-  # aws elbv2 modify-listener --listener-arn "arn:aws:elasticloadbalancing:us-west-2:164348132997:listener/app/THUNKINGSPOTLLC-SVCS/b3d24b7dd2df1dcf/dbb8d3ee6711842b" --default-actions Type=forward,TargetGroupArn="arn:aws:elasticloadbalancing:us-west-2:164348132997:targetgroup/aqua-blue/dc8fe40b380c5fac" --region "us-west-2"
   aws elbv2 modify-listener --listener-arn $INACTIVE_LISTENER_ARN --default-actions Type=forward,TargetGroupArn=$ACTIVE_TARGET_GROUP --region $AWS_REGION
 else
   echo "Invalid phase specified. Exiting..."
